daafffd6f4829f5fe55081354af437c8154afb0e
JaHyeon_Gu <jahyungu@naver.com>
2023-03-10 23:09:14 +0900

    delete Django & test Flask

--- DJ/cb/chat/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def home(request):
-    return render(request, "home.html")
-
-def startchat(request):
-    return render(request, "startchat.html")
