55548660d3827e7c4e31092be60c46d566e5288f
JaHyeon_Gu <jahyungu@naver.com>
2023-03-14 23:51:59 +0900

    making post

--- NF/app.py
+++ NF/app.py
@@ -1,13 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from collections import deque
+from saved.model import *
+
 app = Flask(__name__)
+
+# model = transformer(
+#     vocab_size=VOCAB_SIZE,
+#     num_layers=NUM_LAYERS,
+#     dff=DFF,
+#     d_model=D_MODEL,
+#     num_heads=NUM_HEADS,
+#     dropout=DROPOUT)
+
+# model.load_weights("/Users/jahyeon_gu/WDTT/NF/saved/testmodel_w")
+
+received = deque(maxlen=6)
+send = deque(maxlen=5)
 
 @app.route('/')
 def base():
+    received.clear()
+    send.clear()
+    received.append("안녕하세요! 반가워요!")
     return render_template('base.html')
 
-@app.route('/chat')
+@app.route('/chat', methods=['GET', "POST"])
 def chat():
-    return render_template('chat.html')
+    if request.method == "POST":
+        msg = request.form.values()
+        send.append(msg)
+        return render_template('chat.html', sends=list(send))
+    return render_template("chat.html")
 
 if __name__ == '__main__':
     app.run()
